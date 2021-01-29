8ad5cddc4c9229d64fb7ad2c0c883b4073e8ff2f
darkhz <kmachanwenw@gmail.com>
2021-01-29 10:47:33 +0530

    sched/fair: Modify boosted_task_util() to reflect uclamp changes
    This is a commit that reflects:
    ANDROID: sched/fair: EAS: Add uclamp support to find_energy_efficient_cpu()
    
    https://github.com/neverware/kernel/commit/3a5e1534e01421a455df2d3f4daaef1b6da54f30
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6758,12 +6758,23 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 static inline unsigned long
 boosted_task_util(struct task_struct *task)
 {
+
+#ifdef CONFIG_UCLAMP_TASK_GROUP
+	unsigned long util = task_util(task);
+	unsigned long util_min = uclamp_eff_value(task, UCLAMP_MIN);
+	unsigned long util_max = uclamp_eff_value(task, UCLAMP_MAX);
+
+	return clamp(util, util_min, util_max);
+#else
+
 	unsigned long util = task_util_est(task);
 	long margin = schedtune_task_margin(task);
 
 	trace_sched_boost_task(task, util, margin);
 
 	return util + margin;
+#endif
+#endif
 }
 
 static unsigned long cpu_util_without(int cpu, struct task_struct *p);
@@ -7523,6 +7534,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if (is_reserved(i))
 				continue;
 
+<<<<<<< HEAD
 			if (sched_cpu_high_irqload(i))
 				continue;
 
@@ -7532,6 +7544,12 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if (sched_boost_top_app() && rd->max_cap_orig_cpu != -1 &&
 				(i >= rd->max_cap_orig_cpu && p->prio > DEFAULT_PRIO))
 				break;
+=======
+			/* Skip CPUs which do not fit task requirements */
+			if (capacity_of(i) < boosted_task_util(p))
+				continue;
+
+>>>>>>> 41057c102d98 (sched/fair: Modify boosted_task_util() to reflect uclamp changes)
 			/*
 			 * p's blocked utilization is still accounted for on prev_cpu
 			 * so prev_cpu will receive a negative bias due to the double
